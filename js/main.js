be444582252fa2d277e3f9380877f937f23991cd
zhangxiaoshun <15010969174@163.com>
2018-05-16 15:34:55 +0800

    样式修改

--- js/main.js
+++ js/main.js
@@ -6,20 +6,21 @@ $(function () {
         let _this=$(this);
         let _border=$("#border-h");
         _this.find(".men-list").addClass("active");
+        _this.find(".men-list-img").addClass("active");
         let index=_this.index();
         if(index<1){
-            _border.css("width","0.7rem").css("left","0.8rem");
+            _border.css("width","0.7rem").css("left","0.5rem");
         }else{
             _border
                 .css("width","0.7rem")
                 .css("left",{
-                    "1":"2.55rem",
-                    "2":"4.35rem",
-                    "3":"6rem",
-                    "4":"7.5rem",
-                    "5":"9.2rem",
-                    "6":"11rem",
-                    "7":"12.3rem",
+                    "1":"2rem",
+                    "2":"3.8rem",
+                    "3":"5.32rem",
+                    "4":"7rem",
+                    "5":"8.6rem",
+                    "6":"10.4rem",
+                    "7":"12.5rem",
                 }[index]);
         }
 
@@ -28,6 +29,7 @@ $(function () {
         let _border=$("#border-h");
         _border.css("width","0rem").css("left","0.8rem");
         _this.find(".men-list").removeClass("active");
+        _this.find(".men-list-img").removeClass("active");
     })
 
 
